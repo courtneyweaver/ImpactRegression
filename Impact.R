78642b7cdb7890f4d593a935a7423e42d1d75f4e
Courtney Weaver <48768086+courtneyweaver@users.noreply.github.com>
2019-04-11 11:54:06 -0400

    Created tables using Stargazer

--- Impact.R
+++ Impact.R
@@ -12,6 +12,7 @@ library(lmtest)
 library(sandwich)
 library(pastecs)
 library(tibble)
+library(stargazer)
 
 # import data
 data <- read_xlsx("Master.xlsx")
@@ -27,7 +28,7 @@ names(data)[5] <- "sales"
 data <- data[!duplicated(data[c("year", "ID")]),]
 
 # delete all data with incomplete values
-data <- data[!is.na(data$hours),!is.na(data$employees),]
+data <- data[!is.na(data$hours),]
 data <- data[!is.na(data$employees),]
 
 # delete zero or NA sales
@@ -52,38 +53,12 @@ panel <- plm(lnSales ~ hours + employees + hoursSq, data=data, index=c("ID", "ye
 summary(panel)
 
 # print summary 
-tbl <- tidy(panel)
-kable(tbl, digits=5, caption=
-        "Estimation of Sales using Fixed Effects")%>%
-  kable_styling(bootstrap_options = "striped", full_width = F)
+stargazer(panel, type="html",
+          dep.var.labels=c("Sales"),
+          out="models.txt")
 
+# print summary statistics
+stargazer(as.data.frame(data[c("hours","employees","sales")]), type="text",
+          summary.stat= c("n","mean", "sd", "min","max"), out = "SS.txt", digits = 2)
 
-# print summary using robust standard errors
-a <- coeftest(panel, vcov. = vcovHC, type = "HC1")
-b <- tidy(a)
-kable(b, digits=5, caption=
-        "Estimation of Sales using Fixed Effects with Robust Standard Errors")%>%
-  kable_styling(bootstrap_options = "striped", full_width = F)
-
-# summary statistics on control variables
-options(scipen=100)
-options(digits=1)
-c <- stat.desc(data)
-d <-t(c)
-d <- as.data.frame(d)
-
-# drop columns
-e <- d[ -c(2:3,6:8,10:12,14) ] 
-
-# drop rows
-f <- e[-c(1:2,6), ]
-row <- row.names(f)
-rows <- str_to_title(row)
-g <- cbind(rows,f$nbr.val, f$min, prettyNum((f$max),big.mark=",", scientific=FALSE), 
-    prettyNum((f$mean),big.mark=",", scientific=FALSE), prettyNum((f$std.dev),big.mark=",", scientific=FALSE))
-
-# create table of summary statistics
-kable(g, col.names = c("Variable", "Obs", "Min","Max", "Mean", "Std Dev"), 
-      caption = "Summary Statistics") %>% 
-  kable_styling(bootstrap_options = "basic", full_width = F, position = "center")
 
